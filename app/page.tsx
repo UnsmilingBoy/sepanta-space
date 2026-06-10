e932c5f5b208ea44faf6555f473e1a87b13b5d78
UnsmilingBoy <sepantashafizadeh@gmail.com>
2026-06-10 14:27:30 +0330

    Home updated.

--- app/page.tsx
+++ app/page.tsx
@@ -11,27 +11,41 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel"
 import { Card, CardContent } from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+import { Separator } from "@/components/ui/separator"
 
 export default function Page() {
   const typedName = useTypingAnimation("Sepanta Shafizadeh", 80, 100)
 
   return (
-    <div className="flex min-h-svh justify-center p-6">
-      <div className="flex w-full max-w-3xl min-w-0 flex-col gap-4 text-sm leading-loose">
+    <div className="m-auto flex min-h-svh w-full max-w-3xl min-w-0 flex-col gap-4 px-6 pr-6 pl-6 text-sm leading-loose">
+      <div className="sticky top-0 z-10 pt-6">
         <FadeIn direction="down">
           <ResumeHeader />
         </FadeIn>
-        <div className="flex justify-between">
-          <div className="mt-10 flex flex-col items-start gap-4">
-            <h1 className="text-3xl">{typedName}</h1>
-            <FadeIn direction="down" delay={1.6}>
-              <span className="bg-foreground p-1 text-sm font-bold text-background">
+      </div>
+      <div className="flex flex-col justify-between gap-10 sm:flex-row">
+        <div className="mt-10 flex flex-col items-start gap-3">
+          <h1 className="text-3xl">{typedName}</h1>
+          <FadeIn direction="down" delay={1.6}>
+            <div className="flex flex-col gap-8">
+              <span className="w-fit bg-foreground p-1 text-sm font-bold text-background">
                 web & mobile developer
                 <span className="animate-caret-blink">_</span>
               </span>
-            </FadeIn>
-          </div>
-          <Carousel className="w-full sm:max-w-xs">
+              <p>
+                Sub-headline: A 1-2 sentence value proposition explaining what
+                you excel at and what drives you.
+              </p>
+              <div className="flex flex-row gap-3">
+                <Button variant={"default"}>Contact Me</Button>
+                <Button variant={"outline"}>View My Work</Button>
+              </div>
+            </div>
+          </FadeIn>
+        </div>
+        <FadeIn direction="left" delay={1.0}>
+          <Carousel className="w-full overflow-hidden sm:max-w-xs">
             <CarouselContent>
               {Array.from({ length: 5 }).map((_, index) => (
                 <CarouselItem key={index}>
@@ -47,11 +61,14 @@ export default function Page() {
                 </CarouselItem>
               ))}
             </CarouselContent>
-            <CarouselPrevious />
-            <CarouselNext />
+            <CarouselNext size={"xs"} variant={"ghost"} />
+            <CarouselPrevious size={"xs"} variant={"ghost"} />
           </Carousel>
-        </div>
+        </FadeIn>
       </div>
+      <FadeIn direction="up" delay={1.8}>
+        <Separator />
+      </FadeIn>
     </div>
   )
 }
